10bee3e4a3405722fa74e1b4df50d67d0bc45819
caicancan <18062411472@163.com>
2018-11-28 19:19:38 +0800

    修改首页进行数据绑定

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,39 +1,58 @@
 // pages/home/home.js
+const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+      // msg:"这是来自js的文件的msg数据",
+      // num:666,
+      // bl:false,
+      //data属性内的数据可以通过数据的属性名称直接调用早wxml
+      //不要在data内定义函数，否则函数无法被调用
+      swiperArr:[
+        { "image_src":"/upload/banner1.png",},
+        { "image_src": "/upload/banner2.png", },
+        { "image_src": "/upload/banner3.png", }
+      ]
   },
+
+ 
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options);
+    console.log(app.globalurl);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log("onReady");
   },
-
+  /**自定义调用的方法两种写法*/
+  testHandle:function() {
+    console.log("测试事件绑定是否成功");
+  },
+  sonHandle(event){
+    console.log("子元素绑定的事件");
+    console.log(event.currentTarget.dataset.para);
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("onshow");
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("onhide");
   },
 
   /**
